48d22991ab461e4bb7139193c0b6ad9ed6fe88bb
evgenydus <evgeny21dus@gmail.com>
2020-07-14 16:32:18 +0300

    Refactor

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -9,7 +9,7 @@ const LoginForm = () => {
     <form className="flex flex-col">
       <input className={cInput} type="text" placeholder="Ваш логин" />
       <input className={cInput} type="password" placeholder="Пароль" />
-      <Button className="bg-green-400 hover:bg-green-500 mt-4 mx-auto">Войти</Button>
+      <Button className="btn-green mt-4 mx-auto">Войти</Button>
     </form>
   )
 }
--- src/components/UI/Button/index.js
+++ src/components/UI/Button/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import classnames from 'classnames'
 
 const Button = ({ children, className, ...props }) => (
-  <button className={classnames('font-semibold px-4 py-2 rounded', className)} {...props}>
+  <button className={classnames('btn', className)} {...props}>
     {children}
   </button>
 )
