faa1c85d2c0ee931553bf0e7c1597cc33f3cb614
evgenydus <evgeny21dus@gmail.com>
2020-07-20 18:46:13 +0300

    Change edit and remove icons

--- src/components/AdminContent/AdminList.js
+++ src/components/AdminContent/AdminList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { css } from '@emotion/core'
 import { observer } from 'mobx-react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import CardWrapper from '../CardWrapper'
 
 const AdminList = ({ admins }) => (
@@ -8,25 +9,20 @@ const AdminList = ({ admins }) => (
     {admins.map(admin => (
       <CardWrapper key={admin.id} className="relative">
         <div
-          className="absolute flex leading-none right-0 top-0"
+          className="absolute flex items-center leading-none right-0 top-0"
           css={css`
             right: 5px;
             top: 5px;
           `}
         >
-          <div
-            className="cursor-pointer hover:bg-gray-300 mr-1 opacity-75 rounded"
-            css={css`
-              transform: rotate(90deg);
-            `}
-          >
-            &#9998;
+          <div className="cursor-pointer hover:bg-gray-300 mr-2 opacity-75 rounded ">
+            <FontAwesomeIcon icon={['far', 'pencil']} />
           </div>
           <div
             className="cursor-pointer hover:bg-red-300 opacity-75 rounded"
             onClick={admin.destroy}
           >
-            ✖
+            <FontAwesomeIcon icon={['far', 'times']} size="lg" />
           </div>
         </div>
         <div className="mb-1 text-sm">{admin.username}</div>
