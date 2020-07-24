45d510b701fbecad872de60ccdf67d5e6dbe461a
evgenydus <evgeny21dus@gmail.com>
2020-07-24 18:37:35 +0300

    Rebuild popover and move colorpicker to popover

--- src/components/UI/ColorPicker.js
+++ src/components/UI/ColorPicker.js
@@ -1,35 +1,29 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { css } from '@emotion/core'
 import { observer } from 'mobx-react'
 import { GithubPicker } from 'react-color'
 import classnames from 'classnames'
+import Popover from './Popover'
 
 const ColorPicker = ({ onChange, value }) => {
-  const [isOpen, setOpen] = useState(false)
-  const toggleOpen = () => setOpen(!isOpen)
-
   const handleChange = ({ hex }) => {
-    toggleOpen()
     if (!onChange) return
     onChange(hex)
   }
 
   return (
-    <div className="relative">
-      <div
-        className={classnames('cursor-pointer h-8 input-text w-8', {
-          'border-primary hover:border-primary': isOpen,
-        })}
-        css={css`
-          background: ${value};
-        `}
-        onClick={toggleOpen}
-      />
-      {isOpen && (
-        <div className="absolute z-50" css={css` top: 2.5rem; `}>
-          <GithubPicker onChange={handleChange} />
-        </div>
-      )}
+    <div>
+      <Popover
+        content={<GithubPicker onChange={handleChange} triangle="hide" width="187px" />}
+        isTransparent
+      >
+        <div
+          className={classnames('cursor-pointer h-8 input-text w-8 hover:border-primary')}
+          css={css`
+            background: ${value};
+          `}
+        />
+      </Popover>
     </div>
   )
 }
--- src/components/UI/Popover.js
+++ src/components/UI/Popover.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import Tippy from '@tippyjs/react'
+import 'tippy.js/animations/shift-away-extreme.css'
 import 'tippy.js/dist/tippy.css'
-import 'tippy.js/themes/translucent.css'
+import 'tippy.js/themes/light.css'
 import classnames from 'classnames'
 
-const Popover = ({ content, children, className }) => (
-  <Tippy content={content} interactive theme="translucent">
+const Popover = ({ children, className, content, isTransparent }) => (
+  <Tippy
+    animation="shift-away-extreme"
+    arrow={false}
+    className={classnames(isTransparent && 'bg-transparent')}
+    content={content}
+    interactive
+    offset={[0, 5]}
+    theme={!isTransparent && 'light'}
+    trigger="click"
+  >
     <div className={classnames(className, 'inline-block')}>{children}</div>
   </Tippy>
 )
