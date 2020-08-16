cf7373d00996ba2d87dc65897fa7a8c7be1c8541
Aleksandr Dus <aleksandr.dus@scompler.com>
2020-08-16 19:29:32 +0800

    Move toast container

--- src/containers/People.js
+++ src/containers/People.js
@@ -3,12 +3,10 @@ import { observer } from 'mobx-react'
 
 import PageWrapper from '../components/PageWrapper'
 import UsersContent from '../components/UsersContent'
-import { ToastContainer } from 'react-toastify'
 
 const People = ({ store }) => (
   <PageWrapper>
     <UsersContent store={store} />
-    <ToastContainer />
   </PageWrapper>
 )
 
