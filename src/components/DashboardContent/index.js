241ca098190aa72bb5ef98e3f4ed71649d8bdf86
evgenydus <evgeny21dus@gmail.com>
2020-08-26 21:31:26 +0300

    Add loader to dashboard

--- src/components/DashboardContent/index.js
+++ src/components/DashboardContent/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import { observer } from 'mobx-react'
-import DashCardsList from './DashCardsList'
 
-const DashboardContent = ({ store }) => (
-  <div className="p-4">
-    <DashCardsList store={store} />
-  </div>
-)
+import DashCardsList from './DashCardsList'
+import LoadIndicator from '../LoadIndicator'
+
+const DashboardContent = ({ store }) => {
+  if (!store.isReady) {
+    return (
+      <div className="inline-block p-4 xs:block xs:mx-auto">
+        <LoadIndicator />
+      </div>
+    )
+  }
+
+  return (
+    <div className="p-4">
+      <DashCardsList store={store} />
+    </div>
+  )
+}
 
 export default observer(DashboardContent)
