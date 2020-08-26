04bfdce952fb5e3077487e5b74d38c20297d22d8
evgenydus <evgeny21dus@gmail.com>
2020-08-26 21:31:26 +0300

    Add messages card to dashboard

--- src/components/DashboardContent/DashCardsList.js
+++ src/components/DashboardContent/DashCardsList.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import { observer } from 'mobx-react'
+
 import CardWrapper from '../CardWrapper'
 
-const DashCardsList = ({ store }) => {
-  return (
-    <div className="grid gap-4 grid-cols-4 text-center">
-      <CardWrapper>
-        <div>Всего пользователей:</div>
-        <div className="text-3xl text-primary">{store.totalUsersCount}</div>
-      </CardWrapper>
-      <CardWrapper className="flex-col inline-flex">
-        <div>Всего групп:</div>
-        <div className="text-3xl text-primary">{store.totalGroupsCount}</div>
-      </CardWrapper>
-    </div>
-  )
-}
+const DashCardsList = ({ store }) => (
+  <div className="grid gap-4 grid-cols-4 text-center">
+    <CardWrapper>
+      <div>Всего пользователей:</div>
+      <div className="text-3xl text-primary">{store.totalUsersCount}</div>
+    </CardWrapper>
+    <CardWrapper className="flex-col inline-flex">
+      <div>Всего групп:</div>
+      <div className="text-3xl text-primary">{store.totalGroupsCount}</div>
+    </CardWrapper>
+    <CardWrapper className="flex-col inline-flex">
+      <div>Всего сообщений:</div>
+      <div className="text-3xl text-primary">{store.totalMessagesCount}</div>
+    </CardWrapper>
+  </div>
+)
 
 export default observer(DashCardsList)
