98378a7ae70c96749338d03afd0045e816691163
evgenydus <evgeny21dus@gmail.com>
2020-09-07 00:47:14 +0300

    Customize modal styling

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,9 +3,14 @@ import { observer } from 'mobx-react'
 import ReactModal from 'react-modal'
 
 ReactModal.setAppElement('#root')
+ReactModal.defaultStyles.overlay.backgroundColor = 'rgba(0, 0, 0, .5)'
 
 const Modal = ({ children, isOpen, close }) => (
-  <ReactModal isOpen={isOpen} onRequestClose={close}>
+  <ReactModal
+    className="-translate-x-1/2 -translate-y-1/2 absolute left-1/2 max-w-sm top-1/2 transform w-11/12"
+    isOpen={isOpen}
+    onRequestClose={close}
+  >
     {children}
   </ReactModal>
 )
